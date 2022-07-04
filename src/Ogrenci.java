3b1f1bf5db44430c7ab4a4d38ed194768933a899
OkanEmreO <okan.emre.ist@gmail.com>
2022-07-04 23:56:05 +0300

    Öğrenci Classı ikinci kısım yapıldı.

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,7 +1,8 @@
 public class Ogrenci {
     public static void main(String[] args) {
 
-        System.out.println( "Merhaba Dünya");
+        System.out.println("Merhaba Dünya 2");
+        System.out.println("Merhaba Dünya 3");
 
 
 
