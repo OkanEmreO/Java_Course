e3b1c0a2025ee9609d1f42bbb6db0b53c49fb55c
OkanEmreO <okan.emre.ist@gmail.com>
2022-07-05 12:54:53 +0300

    İkinci işlem gerçekleşti.

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -2,7 +2,7 @@ public class Ogrenci {
     public static void main(String[] args) {
 
         System.out.println("Merhaba Yeni Dünya 2");
-        System.out.println("Merhaba Yeni Dünya 3");
+        System.out.println("Merhaba Obiwan Kenobi");
 
 
 
